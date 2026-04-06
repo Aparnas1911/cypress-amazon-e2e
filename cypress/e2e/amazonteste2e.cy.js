9357a4c15053c66fd7af1cb3c9acd9c18ed4bc91
Aparna S <aparnasanthosh1119@gmail.com>
2026-04-05 22:02:58 -0400

    changed code since failing in CI/CD

--- cypress/e2e/amazonteste2e.cy.js
+++ cypress/e2e/amazonteste2e.cy.js
@@ -1,30 +1,53 @@
-describe('Amazone Site E2E Test - Search, Product, Cart, Checkout', () => {
- beforeEach(() => {
-    cy.visit('https://www.amazon.ca/')})
-     
-    //Handling pop-up
-    it('Search -> Select Product -> Add to cart -> Checkout',() => {
-    cy.get('body').then(($body) => {
-    if ($body.find('#sp-cc-accept').length) {
-    cy.get('#sp-cc-accept').click()
-    }
+describe('Amazon Site E2E Test - Search, Product, Cart, Checkout', () => {
 
-    //Product Search
-    cy.get('#twotabsearchtextbox').should('be.visible').type('iphone 16 pro max')
-    cy.get('#nav-search-submit-button').click()
-
-    //Selecting the first product
-    cy.get('[data-component-type="s-search-result"] h2').first().click() 
-    
-    //Verifying the product details page
-    cy.get('#feature-bullets').should('contain','About this item')
-
-    //Add to cart
-    cy.get('#add-to-cart-button').should('be.visible').click()
-
-    //Proceed to checkout
-    cy.get('#sc-buy-box-ptc-button').should('be.visible').click()
+  beforeEach(() => {
+    cy.visit('https://www.amazon.ca/', {
+      failOnStatusCode: false
     })
-    }
-    )
+
+    cy.viewport(1280, 720)
+
+    // wait for page to load
+    cy.get('body', { timeout: 20000 }).should('be.visible')
+
+    // handle cookie popup safely
+    cy.get('body').then(($body) => {
+      if ($body.find('#sp-cc-accept').length) {
+        cy.get('#sp-cc-accept').click()
+      }
+    })
+  })
+
+  it('Search -> Select Product -> Add to cart -> Checkout', () => {
+
+    // 🔍 Product Search
+    cy.get('#twotabsearchtextbox', { timeout: 20000 })
+      .should('be.visible')
+      .type('iphone 16 pro max{enter}')
+
+    // wait for results to load
+    cy.get('[data-component-type="s-search-result"]', { timeout: 20000 })
+      .should('have.length.greaterThan', 0)
+
+    // 🛒 Select first product
+    cy.get('[data-component-type="s-search-result"] h2')
+      .first()
+      .should('be.visible')
+      .click()
+
+    // 📄 Verify product page
+    cy.get('#feature-bullets', { timeout: 20000 })
+      .should('be.visible')
+      .and('contain.text', 'About this item')
+
+    // ➕ Add to cart
+    cy.get('#add-to-cart-button', { timeout: 20000 })
+      .should('be.visible')
+      .click()
+
+    // 💳 Proceed to checkout
+    cy.get('#sc-buy-box-ptc-button', { timeout: 20000 })
+      .should('be.visible')
+      .click()
+  })
 })
